7b30a7f096e632b4571afb71b65f1184c306087e
jamesmedice <U0I2747 tiago.medice@gft.com>
2019-11-12 15:49:20 +0100

    ref

--- src/main/java/com/medici/app/Application.java
+++ src/main/java/com/medici/app/Application.java
@@ -2,7 +2,11 @@ package com.medici.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author a73s
+ *
+ */
 @SpringBootApplication
 public class Application {
 
